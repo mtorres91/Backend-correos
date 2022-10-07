8d50b88dde1d67f11f3bd9fbf878e53a19fee1ee
mtorres91 <m.a.t.a.9125_@hotmail.com>
2022-10-07 18:31:09 -0500

    cambios

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 let cors = require('cors');
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+services.AddCors(options =>
 
+    {
+    
+    options.AddPolicy(
+    
+    name, "AllowOrigin",
+    
+    builder => {
+    
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    
+    });
+    
+    });
     
 
 const bodyparser = require('body-parser');
 
 
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+app.UseCors("AllowOrigin");
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
