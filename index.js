071aada269aef8ee75d46e6d17ac0a2bdc31046e
mtorres91 <m.a.t.a.9125_@hotmail.com>
2022-10-07 17:40:48 -0500

    cambios

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 let cors = require('cors');
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+
 const bodyparser = require('body-parser');
 
 
 
+app.use(cors(corsOptions)) 
 
-app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(require('./routes/correoRoutes'));
 
 app.listen('3000', ()=>{
-    
+
     console.log('escuchando');
 })
