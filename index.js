737225ca48bb290174f06b1f25181c4299b93672
mtorres91 <m.a.t.a.9125_@hotmail.com>
2022-10-07 18:18:41 -0500

    cambios

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const app = express();
+let cors = require('cors');
 const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
-var cors = require('cors')
 
-var app = express()
-app.use(cors())
+    
 
 const bodyparser = require('body-parser');
 
 
 
 app.use(cors(corsOptions)) 
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();});
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
