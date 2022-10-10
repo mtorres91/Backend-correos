d241831adbe2896a87c185b77c8243d6421a204f
mario <mario>
2022-10-09 23:32:50 -0500

    s

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const express = require('express');
 const app = express();
 let cors = require('cors');
     
-app.use(
-    cors({
-        origin: "*"
-    }
-));
+
 
 const bodyparser = require('body-parser');
 
 
-
+app.use(express.json());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(require('./routes/correoRoutes'));
-
+app.use(cors());
 app.get('/',(req,res)=>
     {res.send('hello world')
 
