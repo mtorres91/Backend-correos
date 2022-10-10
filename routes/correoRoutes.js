fcd005c276d797df62b5398cf5ee2c04b77486df
mario <mario>
2022-10-09 23:35:38 -0500

    s

--- routes/correoRoutes.js
+++ routes/correoRoutes.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 let cors = require('cors');
 
-app.use(
-    cors({
-        origin: "*"
-    }
-));
+
 let envio = require('../controllers/correoController');
+
+app.use(cors());
 
 app.post('/envio',envio.envioCorreo);
 
